3c25f8363e8dfa6cdc62591a2ada8272c4533055
Imabhinavvv <abhinavmamidi.mail@gmail.com>
2025-06-05 18:07:00 +0530

    Deleted all existing files

--- checkCam.py
+++
@@ -1,8 +0,0 @@
-import cv2
-
-cap = cv2.VideoCapture(0)  # 0 is usually the default camera
-if not cap.isOpened():
-    print("Error: Camera not found or could not be opened.")
-else:
-    print("Camera is working!")
-    cap.release()
